fd9079e5244bbcf4206e8cfa66d7f31590aa620e
jonatan <jonatancotoc@gmail.com>
2016-11-15 05:57:07 +0100

    templates

--- post/urls.py
+++ post/urls.py
@@ -1,13 +1,19 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from . import views
+from .views import (
+    post_list,
+    post_create,
+    post_detail,
+    post_update,
+    post_delete,
+    )
 
 urlpatterns = [
-    url(r'^$', "post.views.post_list"),
-    url(r'^create/$', "post.views.post_create"),
-    url(r'^detail/$', "post.views.post_detail"),
-    url(r'^update/$', "post.views.post_update"),
-    url(r'^delete/$', "post.views.post_delete"),
+    url(r'^$', post_list),
+    url(r'^create/$', post_create),
+    url(r'^detail/(?P<id>\d+)/$', post_detail),
+    url(r'^update/$', post_update),
+    url(r'^delete/$', post_delete),
     #url(r'^post/$', <appname>.views.<function_name>"),
 ]
--- post/views.py
+++ post/views.py
@@ -1,16 +1,35 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-
-def post_list(request): #list itmens
-    return HttpResponse("<h1>List</h1>")
+from .models import Post
 
 def post_create(request):
     return HttpResponse("<h1>Create</h1>")
 
-def post_detail(request): #retrive
-    return HttpResponse("<h1>Detail</h1>")
+def post_detail(request, id=None): #retrive
+    instance = get_object_or_404(Post, id=2)
+    context = {
+        "title":instance.title,
+        "instance": instance
+    }
+    return render(request, "post_detail.html", context)
+
+def post_list(request): #list itmens
+    queryset = Post.objects.all()
+    context = {
+        "objects_list": queryset,
+        "title":"List"
+        }
+    #if request.user.is_authenticated():
+    #    context = {
+    #        "title":"My User List"
+    #    }
+    #else:
+    #    context = {
+    #        "title":"List"
+    #    }
+    return render(request, "index.html", context)
 
 def post_update(request):
     return HttpResponse("<h1>Update</h1>")
