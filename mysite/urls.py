53a4f1d8d42b565e1d559a4874cbeb97f0119ad5
jonatan <jonatancotoc@gmail.com>
2016-11-17 07:53:25 +0100

    mensajes css y bootstrap

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
     2. Import the include() function: from django.conf.urls import url, include
     3. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 #from contact import views as contact_view
@@ -23,6 +25,9 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     #url(r'^post/$', post_view.post_home),
-    url(r'^post/', include("post.urls")),
+    url(r'^post/', include("post.urls", namespace='post')),
     #url(r'^post/$', <appname>.views.<function_name>"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- post/views.py
+++ post/views.py
@@ -1,38 +1,66 @@
-from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404, redirect
 
-# Create your views here.
+from .forms import PostForm
 from .models import Post
 
 def post_create(request):
-    return HttpResponse("<h1>Create</h1>")
+    form = PostForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        #messages success
+        messages.success(request, "Guardado")
+        return HttpResponseRedirect(instance.get_absolute_url())
+    else:
+        messages.error(request, "No Guardado")
+    #if request.method == "POST":
+    #    print ("title" + request.POST.get("content"))
+    #    print (request.POST.get("title"))
+    #    #Post.objects.create(title=title)
+    context = {
+        "form": form,
+    }
+    return render(request, "post_form.html", context)
 
 def post_detail(request, id=None): #retrive
-    instance = get_object_or_404(Post, id=2)
+    instance = get_object_or_404(Post, id=id)
     context = {
         "title":instance.title,
         "instance": instance
     }
     return render(request, "post_detail.html", context)
 
-def post_list(request): #list itmens
+def post_list(request):
     queryset = Post.objects.all()
     context = {
         "objects_list": queryset,
         "title":"List"
         }
-    #if request.user.is_authenticated():
-    #    context = {
-    #        "title":"My User List"
-    #    }
-    #else:
-    #    context = {
-    #        "title":"List"
-    #    }
-    return render(request, "index.html", context)
 
-def post_update(request):
-    return HttpResponse("<h1>Update</h1>")
+    return render(request, "post_list.html", context)
 
-def post_delete(request):
-    return HttpResponse("<h1>Delete</h1>")
+def post_update(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        messages.success(request, "Se Edito correctamente")
+        #messages success
+        return HttpResponseRedirect(instance.get_absolute_url())
+
+    context = {
+        "title":instance.title,
+        "instance": instance,
+        "form": form,
+    }
+    return render(request, "post_form.html", context)
+
+
+def post_delete(request, id=None):
+    instance = get_object_or_404(Post, id=id)
+    instance.delete()
+    messages.success(request, "Se Elimino correctamente")
+    return redirect("post:list")
