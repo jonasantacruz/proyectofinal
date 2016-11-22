e2432df26262f5ecc03308d54245feb1981672fd
jonatan <jonatancotoc@gmail.com>
2016-11-23 00:14:22 +0100

    cambio

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,4 +132,4 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
-LOGIN_REDIRECT_URL = reverse_lazy('post.url:detail')
+LOGIN_REDIRECT_URL = 'post/'
